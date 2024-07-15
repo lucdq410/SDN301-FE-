b9559a8f5c53667840f2a7bcc49f1f9b1fcd8b22
viet <viethxse@gmail.com>
2024-07-15 13:14:45 +0700

    ticket

---
+++ src/services/slotPickerService.js
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { apiURL } from "../configs";
+const slotPickerService = () => {
+  const api = apiURL + "/slotPicker";
+  return {};
+};
+
+export default slotPickerService;
